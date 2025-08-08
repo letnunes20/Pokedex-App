cafddf9c21810e3b55ec9de64157fd46dad6b27f
letnunes20 <letcanunes@gmail.com>
2025-08-08 11:38:27 -0300

    pág 50 (Arrumar alguma coisa

--- pokedex/app/index.jsx
+++ pokedex/app/index.jsx
@@ -1,10 +1,12 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import Header from '@/components/Header';
 import { pokemons } from "@/data/pokemons"
+import Card from "../components/Card";
 
 export default function Index() {
+
    const renderPokemon = ({ item }) => (
-    <Text>{item.Nome}</Text>
+    <Card pokemon={item}/>
    )
 
   return (
--- pokedex/components/Card/index.jsx
+++ pokedex/components/Card/index.jsx
@@ -1,3 +1,26 @@
 import { Image, View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { POKEMON_TYPE_COLORS } from "../../utils/colors";
 import { styles } from "./styles";
+import Button from "../Button";
+
+export default function Card({ pokemon }) {
+    return (
+        <TouchableOpacity
+            style={[styles.container,
+                { backgroundColor: `${POKEMON_TYPE_COLORS[pokemon.Tipo[0].Nome]}` },
+            ]}>
+            <View style={styles.info}>
+                <Text style={styles.numero}>#{pokemon.Numero}</Text>
+                <Text style={styles.nome}>{pokemon.Nome}</Text>
+                <ScrollView horizontal>
+                    {pokemon.Tipo.map((tipo) => (
+                        <Button tipo={tipo} key={tipo.Nome}/>
+                    ))}
+                </ScrollView>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image source={{ uri: pokemon.Image }} style={styles.imagem}></Image>
+            </View>
+        </TouchableOpacity>
+    );
+}
